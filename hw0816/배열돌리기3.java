9305a2fb5685b8dcd661562c802afb0fc03dd78d
kimjungwon <koo_m@naver.com>
2019-08-18 23:51:54 +0000

    Update hw0816/배열돌리기3.java, hw0816/배열돌리기4.java files

---
+++ hw0816/배열돌리기3.java
@@ -0,0 +1,154 @@
+﻿import java.util.Scanner;
+
+public class Main {
+	static Scanner sc = new Scanner(System.in);
+	static int N;
+	static int M;
+	static int dx[] = { -1, 0, 1, 0 };
+	static int dy[] = { 0, -1, 0, 1 };
+	static int Map[][];
+	static int temp[][];
+
+	static void rotation(int R, int C) {
+
+		if (C == 1) {
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= M; j++) {
+					temp[i][j] = Map[N - i + 1][j];
+				}
+			}
+		} else if (C == 2) {
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= M; j++) {
+					temp[i][j] = Map[i][M - j + 1];
+				}
+			}
+		} else if (C == 3) // 오른쪽으로 90도 회전.
+		{
+			int te = N;
+			N = M;
+			M = te;
+			for (int i = 1; i <= N; i++) // 4
+			{
+				for (int j = 1; j <= M; j++) // 3
+				{
+					temp[i][j] = Map[M - j + 1][i];
+				}
+			}
+		} else if (C == 4) // 왼쪽으로 90도 회전.
+		{
+			int te = N;
+			N = M;
+			M = te;
+			for (int i = 1; i <= N; i++) // 4
+			{
+				for (int j = 1; j <= M; j++) // 3
+				{
+					temp[i][j] = Map[j][N + 1 - i];
+				}
+			}
+		} else if (C == 5) // 이건 뭐지 . .
+		{
+			// 1 2 => 3 1
+			// 3 4 4 2
+			int prev[][] = new int[(N / 2) + 1][(M / 2) + 1];
+			int next[][] = new int[(N / 2) + 1][(M / 2) + 1];
+			for (int i = 1; i <= N / 2; i++) {
+				for (int j = 1; j <= M / 2; j++) {
+					prev[i][j] = Map[i][j]; // 1
+				}
+			}
+
+			// 1 = 3
+			for (int i = 1; i <= N / 2; i++) {
+				for (int j = 1; j <= M / 2; j++) {
+					Map[i][j] = Map[(N / 2) + i][j];
+				}
+			}
+			// 3 = 4
+			for (int i = 1; i <= N / 2; i++) {
+				for (int j = 1; j <= M / 2; j++) {
+					Map[(N / 2) + i][j] = Map[(N / 2) + i][(M / 2) + j];
+				}
+			}
+			// 4 = 2
+			for (int i = 1; i <= N / 2; i++) {
+				for (int j = 1; j <= M / 2; j++) {
+					Map[(N / 2) + i][(M / 2) + j] = Map[i][(M / 2) + j];
+				}
+			}
+			// 2 = 1
+			for (int i = 1; i <= N / 2; i++) {
+				for (int j = 1; j <= M / 2; j++) {
+					Map[i][(M / 2) + j] = prev[i][j];
+				}
+			}
+		} else if (C == 6) {
+			// 1 2 => 2 3
+			// 4 3 1 4
+			int prev[][] = new int[(N / 2) + 1][(M / 2) + 1];
+			int next[][] = new int[(N / 2) + 1][(M / 2) + 1];
+			for (int i = 1; i <= N / 2; i++) {
+				for (int j = 1; j <= M / 2; j++) {
+					prev[i][j] = Map[i][j]; // 1
+				}
+			}
+
+			// 1 = 2
+			for (int i = 1; i <= N / 2; i++) {
+				for (int j = 1; j <= M / 2; j++) {
+					Map[i][j] = Map[i][(M / 2) + j];
+				}
+			}
+			// 2 = 3
+			for (int i = 1; i <= N / 2; i++) {
+				for (int j = 1; j <= M / 2; j++) {
+					Map[i][(M / 2) + j] = Map[(N / 2) + i][(M / 2) + j];
+				}
+			}
+			// 3 = 4
+			for (int i = 1; i <= N / 2; i++) {
+				for (int j = 1; j <= M / 2; j++) {
+					Map[(N / 2) + i][(M / 2) + j] = Map[(N / 2) + i][j];
+				}
+			}
+			// 4 = 1
+			for (int i = 1; i <= N / 2; i++) {
+				for (int j = 1; j <= M / 2; j++) {
+					Map[(N / 2) + i][j] = prev[i][j];
+				}
+			}
+		}
+		if (C != 5 && C != 6) {
+
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= M; j++) {
+					Map[i][j] = temp[i][j];
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		N = sc.nextInt();
+		M = sc.nextInt();
+		int R = sc.nextInt();
+		Map = new int[101][101];
+		temp = new int[101][101];
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= M; j++) {
+				Map[i][j] = sc.nextInt();
+			}
+		}
+		for (int i = 0; i < R; i++) {
+			int C = sc.nextInt();
+			rotation(R, C);
+		}
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= M; j++) {
+				System.out.print(Map[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ hw0816/배열돌리기4.java
@@ -0,0 +1,156 @@
+﻿import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+
+public class Main 
+{
+	static int Minn;
+	static int N , M, K; 
+	static int Map[][];
+	static Point[] user;
+	static ArrayList<Point> L = new ArrayList<>();
+	static int temp[][];
+	static class Point
+	{
+		int x; 
+		int y; 
+		int size; 
+		Point( int x, int y, int size)
+		{
+			this.x = x; 
+			this.y = y;
+			this.size = size; 
+		}
+	}
+	static boolean boundary(int x, int y)
+	{
+		if( x <=0 || y <=0 || x >N || y >M)
+		{
+			return false; 
+		}
+		return true; 
+	}
+	
+	static void dfs(int cnt)
+	{
+		if( cnt == K)
+		{	
+			
+			for (int i = 0; i < user.length; i++) 
+			{
+				rotation(user[i].x, user[i].y, user[i].size);
+			}
+			int sum =0;
+			for(int i=1;i<=N;i++)
+			{
+				sum =0;
+				for(int j =1; j <=M;j++)
+				{
+					sum += Map[i][j]; 
+				}
+				if( Minn > sum)
+				{
+					Minn =sum;
+				}
+			}
+			for(int d=1;d<=N;d++)
+			{
+				for(int j =1; j <=M;j++)
+				{
+					Map[d][j] = temp[d][j]; 
+				}
+			}
+			return; 
+		}
+		int g = L.size();
+		for(int i =0 ; i< g;i++)
+		{		
+			user[cnt] = L.remove(i);
+			dfs(cnt+1);						
+			L.add(i, user[cnt]);
+		}
+	}	
+	static void rotation( int x, int y , int size)
+	{
+		for(int i =1; i<= size;i++)
+		{
+			int nx  = x + i; 
+			int ny  = y + i; 
+			if( boundary(nx,ny)==false)return;
+			int prev = Map[nx][ny];
+			int next = 0; 
+
+			// 왼쪽으로 
+			for(int j =1;j<(2*i+1);j++)	
+			{
+				ny = ny-1;
+				next = Map[nx][ny];
+				Map[nx][ny] = prev; 
+				prev= next; 
+			}
+
+			// 위로 
+			for(int j =1;j<(2*i+1);j++ )
+			{
+				nx = nx-1;
+				next = Map[nx][ny];
+				Map[nx][ny] = prev; 
+				prev = next;
+			}
+			
+			// 오쪽으로. 
+			for(int j =1;j<(2*i+1);j++)	
+			{
+				ny = ny +1;
+				next = Map[nx][ny];
+				Map[nx][ny] = prev; 
+				prev = next;	
+			}
+			
+			// 아래로
+			for(int j =1;j<(2*i+1);j++)	
+			{
+				nx = nx +1;
+				next = Map[nx][ny];
+				Map[nx][ny] = prev; 
+				prev =next; 
+			}
+		}
+	}
+	public static void main(String[] args) throws Exception 
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Minn = Integer.MAX_VALUE;
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		user = new Point[K];
+		Map = new int [N+1][M+1];  
+		temp = new int[N+1][M+1];
+		for(int i=1;i<=N;i++)
+		{
+			st = new StringTokenizer(br.readLine());
+			for(int j =1; j <=M;j++)
+			{
+				Map[i][j] = Integer.parseInt(st.nextToken());
+				temp[i][j] = Map[i][j];
+ 			}
+		}	
+		for(int i =0 ; i < K;i++)  // 연산의 갯수를 의미한다. 
+		{
+			int x,y,size;
+			st = new StringTokenizer(br.readLine());
+            x = Integer.parseInt(st.nextToken()); 
+			y = Integer.parseInt(st.nextToken());
+			size= Integer.parseInt(st.nextToken());
+			L.add(new Point(x,y,size));
+		}
+		dfs(0);
+		System.out.println(Minn);
+	}
+}
