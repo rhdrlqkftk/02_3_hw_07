6885f34dbd7ce159b5fc53accf1ccfdaa22644ad
kimjungwon <koo_m@naver.com>
2019-08-21 11:48:48 +0000

    Update hw0821/모듈러연산.java

---
+++ hw0821/모듈러연산.java
@@ -0,0 +1,19 @@
+package ws0821;
+
+import java.util.Scanner;
+
+public class MOD {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt(); 
+		int B = sc.nextInt();  
+		int C = sc.nextInt(); 
+		System.out.println((A+B)%C);
+		System.out.println((A%C + B%C)%C);
+		System.out.println((A*B)%C);
+		System.out.println( ((A%C) *(B%C))%C);
+	}
+
+}
